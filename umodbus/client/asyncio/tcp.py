2aa2e37341b3adf27bbf6a218f332bc528251c37
Charbel Jacquin <charbeljacquin@gmail.com>
2019-02-13 12:48:06 +0100

    consolidate asyncio/tcp

--- umodbus/client/asyncio/tcp.py
+++ umodbus/client/asyncio/tcp.py
@@ -2,7 +2,7 @@
 """
 import asyncio
 from logzero import logger
-from async_timeout import timeout as async_timeout
+from async_timeout import timeout as aio_timeout
 from umodbus.client.tcp import (
     raise_for_exception_adu,
     expected_response_pdu_size_from_request_pdu,
@@ -10,6 +10,10 @@ from umodbus.client.tcp import (
 )
 import umodbus.client.tcp as tcp
 
+DEBUG = False
+def debug(*args, **kw):
+    if DEBUG:
+        logger.debug(*args, **kw)
 
 async def recv_exactly(reader, size, timeout):
     """ Use the function to read and return exactly number of bytes desired.
@@ -26,8 +30,8 @@ async def recv_exactly(reader, size, timeout):
     recv_bytes = 0
     chunks = []
     while recv_bytes < size:
-        logger.debug(f'awaiting {size - recv_bytes}')
-        async with async_timeout(timeout):
+        debug(f'awaiting {size - recv_bytes}')
+        async with aio_timeout(timeout):
             chunk = await reader.read(size - recv_bytes)
         if len(chunk) == 0:  # when closed or empty
             break
@@ -51,27 +55,28 @@ async def send_message(adu, reader, writer, timeout=None):
     :param sock: Socket instance.
     :return: Parsed response from server.
     """
-    logger.debug(f'sending packet {adu!r}')
-    logger.debug(f'reader: {reader}')
-    logger.debug(f'writer: {writer}')
-    logger.debug(f'timeout: {timeout}')
+    debug(f'sending packet {adu!r}')
+    debug(f'reader: {reader}')
+    debug(f'writer: {writer}')
+    debug(f'timeout: {timeout}')
     status = writer.write(adu)
-    logger.debug(f'status: {status}')
+    await writer.drain()
+    debug(f'status: {status}')
 
     # Check exception ADU (which is shorter than all other responses) first.
     exception_adu_size = 9
-    logger.debug('reading response header')
-    response_error_adu = await recv_exactly(reader, exception_adu_size, timeout)
+    debug('reading response header')
+    async with aio_timeout(timeout):
+        response_error_adu = await reader.readexactly(exception_adu_size)
     raise_for_exception_adu(response_error_adu)
     expected_response_size = \
-        expected_response_pdu_size_from_request_pdu(adu[7:]) + 7
-    logger.debug(f'reading response data ({expected_response_size} bytes)')
-    response_remainder = await recv_exactly(reader,
-                                            expected_response_size - exception_adu_size,
-                                            timeout)
+            expected_response_pdu_size_from_request_pdu(adu[7:]) + 7
+    debug(f'reading response data ({expected_response_size} bytes)')
+    async with aio_timeout(timeout):
+        response_remainder = await reader.readexactly(expected_response_size - exception_adu_size)
     response = response_error_adu + response_remainder
-    logger.debug(f'parsing response {response!r}')
+    debug(f'parsing response {response!r}')
 
     parsed = parse_response_adu(response, adu)
-    logger.debug('done')
+    debug('done')
     return parsed
